8e97965749d249f7f98451bbca1983d82827b386
Kaushik Mishra <kaushik530411@gmail.com>
2018-10-17 18:51:50 -0400

    Stored the intezer 0 in the register  and printed by intezer.

--- PrintByCharacter.asm
+++ PrintByCharacter.asm
@@ -3,3 +3,7 @@ main:
 	li $v0, 11  #  Print by Character
 	li $a0, '@'  #  Stores the character '@', decimal value = 64
 	syscall
+
+	li $v0, 1  #  Print by Intezer
+	add $a0, $a0, -64  #  Stores the intezer 0
+	syscall
